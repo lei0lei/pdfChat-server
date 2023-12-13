01c0bea97c43bae1d5557b22adc1552a8822431c
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-14 01:12:31 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
--- src/message-exchange/message-exchange.controller.ts
+++ src/message-exchange/message-exchange.controller.ts
@@ -8,47 +8,52 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { Document } from "langchain/document";
 
-const openAIApiKey = ''
-// const textSplitter = new RecursiveCharacterTextSplitter({
-//           chunkSize: 1500,
-//           chunkOverlap: 200,
-//         });
-// const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" ,openAIApiKey:openAIApiKey);
-// const embeddings = new OpenAIEmbeddings({openAIApiKey:openAIApiKey);
+const openAIApiKey = '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'
+const textSplitter = new RecursiveCharacterTextSplitter({
+          chunkSize: 1500,
+          chunkOverlap: 200,
+        });
+const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" ,openAIApiKey:openAIApiKey});
+const embeddings = new OpenAIEmbeddings({openAIApiKey:openAIApiKey});
 
 @Controller('messageExchange')
 export class MessageExchangeController {
   @Post('textMessageUpload')
   async receiveTextMessage(@Body() body: any) {
-    console.log(body); // 查看文本消息内容
-    let result = 'no result'
+    // console.log(body); // 查看文本消息内容
+    
     // 这里你可以实现处理和保存消息的逻辑
     // ...
 
-    // const splitDocs = await textSplitter.splitDocuments([
-    //       new Document({ pageContent: body.docs }),
-    //     ])
+    const splitDocs = await textSplitter.splitDocuments([
+          new Document({ pageContent: body.docs }),
+        ])
 
-    // const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
-    // const memory = new BufferMemory({
-    //   memoryKey: "chat_history",
-    //   returnMessages: true,
-    // });
+    const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
+    const memory = new BufferMemory({
+      memoryKey: "chat_history",
+      returnMessages: true,
+    });
 
-    // const chain = ConversationalRetrievalQAChain.fromLLM(
-    //   model,
-    //   vectorStore.asRetriever(),
-    //   {
-    //     memory,
-    //   }
-    // );
-    // const result = await chain.call({
-    //   question: body.quiz,
-    // });
-
+    const chain = ConversationalRetrievalQAChain.fromLLM(
+      model,
+      vectorStore.asRetriever(),
+      {
+        memory,
+      }
+    );
+    let result = await chain.call({
+      question: body.quiz,
+    });
+    if (result == null){
+      return {
+        statusCode: 200,
+        message: 'No result',
+      };}
+     
     return {
       statusCode: 200,
-      message: body.docs,
+      message: result,
     };
   }
 }

8542866027d73784f0a218cce04a22d41b8e425b
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-14 02:37:16 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,13 @@ async function bootstrap() {
   //   origin: 'http://localhost:3000',
   //   credentials: true,
   // });
-  const cors = require('cors');
-  app.use(cors({
-      origin: 'http://localhost:3000', // use your actual domain name (or localhost), using * is not recommended
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-      credentials: true
-  }));
+  // const cors = require('cors');
+  // app.use(cors({
+  //     origin: 'http://localhost:3000', // use your actual domain name (or localhost), using * is not recommended
+  //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  //     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  //     credentials: true
+  // }));
   await app.listen(port);
 }
 bootstrap();

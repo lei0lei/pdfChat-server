3bd3bdfc56b2bc248fdfe6dfee4ddb3bdeac5379
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-13 22:29:05 +0800

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'You are NOT SUPPOSED to access the website directly. lol';
+    return 'Hello World!';
   }
 }

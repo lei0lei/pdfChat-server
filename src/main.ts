51c6f48a720636f50c703c6c6deaa0e5c6adf9ce
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-14 02:26:49 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   // });
   const cors = require('cors');
   app.use(cors({
-      origin: 'http://example.com', // use your actual domain name (or localhost), using * is not recommended
+      origin: 'http://localhost:3000', // use your actual domain name (or localhost), using * is not recommended
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
       credentials: true

679c3427404a00033e74054592eac4c9720296cd
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-14 09:07:12 +0800

    .

--- src/message-exchange/message-exchange.controller.ts
+++ src/message-exchange/message-exchange.controller.ts
@@ -8,13 +8,13 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { Document } from "langchain/document";
 
-// const openAIApiKey = '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'
-// const textSplitter = new RecursiveCharacterTextSplitter({
-//           chunkSize: 1500,
-//           chunkOverlap: 200,
-//         });
-// const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" ,openAIApiKey:openAIApiKey});
-// const embeddings = new OpenAIEmbeddings({openAIApiKey:openAIApiKey});
+const openAIApiKey = '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'
+const textSplitter = new RecursiveCharacterTextSplitter({
+          chunkSize: 1500,
+          chunkOverlap: 200,
+        });
+const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" ,openAIApiKey:openAIApiKey});
+const embeddings = new OpenAIEmbeddings({openAIApiKey:openAIApiKey});
 
 @Controller('messageExchange')
 export class MessageExchangeController {
@@ -25,31 +25,31 @@ export class MessageExchangeController {
     // 这里你可以实现处理和保存消息的逻辑
     // ...
 
-    // const splitDocs = await textSplitter.splitDocuments([
-    //       new Document({ pageContent: body.docs }),
-    //     ])
+    const splitDocs = await textSplitter.splitDocuments([
+          new Document({ pageContent: body.docs }),
+        ])
 
-    // const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
-    // const memory = new BufferMemory({
-    //   memoryKey: "chat_history",
-    //   returnMessages: true,
-    // });
+    const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
+    const memory = new BufferMemory({
+      memoryKey: "chat_history",
+      returnMessages: true,
+    });
 
-    // const chain = ConversationalRetrievalQAChain.fromLLM(
-    //   model,
-    //   vectorStore.asRetriever(),
-    //   {
-    //     memory,
-    //   }
-    // );
-    // let result = await chain.call({
-    //   question: body.quiz,
-    // });
-    // if (result == null){
-    //   return {
-    //     statusCode: 200,
-    //     message: 'No result',
-    //   };}
+    const chain = ConversationalRetrievalQAChain.fromLLM(
+      model,
+      vectorStore.asRetriever(),
+      {
+        memory,
+      }
+    );
+    let result = await chain.call({
+      question: body.quiz,
+    });
+    if (result == null){
+      return {
+        statusCode: 200,
+        message: 'No result',
+      };}
      
     return {
       statusCode: 200,

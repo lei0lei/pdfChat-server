bbf02e20b2c7a84d37516da55ac987995e81559f
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-19 22:05:07 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const cors = require('cors');
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
